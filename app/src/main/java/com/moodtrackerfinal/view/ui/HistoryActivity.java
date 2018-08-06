72e90d1c137e71087643a626b3ec5e8fc519ced6
EzekielDeAngelo <anthony.p777@gmail.com>
2018-08-07 00:47:13 +0200

    History navigation

---
+++ app/src/main/java/com/moodtrackerfinal/view/ui/HistoryActivity.java
@@ -0,0 +1,16 @@
+package com.moodtrackerfinal.view.ui;
+
+import android.os.Bundle;
+
+import com.moodtrackerfinal.R;
+
+public class HistoryActivity extends MainActivity
+{
+    protected void onCreate(Bundle savedInstanceState)
+
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+    }
+}
--- app/src/main/java/com/moodtrackerfinal/view/ui/MainActivity.java
+++ app/src/main/java/com/moodtrackerfinal/view/ui/MainActivity.java
@@ -1,13 +1,36 @@
-package com.moodtrackerfinal;
+package com.moodtrackerfinal.view.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import com.moodtrackerfinal.R;
+
+public class MainActivity extends AppCompatActivity
+{
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
+    }
+
+    private void initUI()
+    {
+
+    }
+    public void showHistory(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+        startActivity(intent);
+    }
+    public void addNote(View view)
+    {
+        //addDailyNote();
     }
 }
